ab6e5afa68921f1cd4ec5d222cbd736abf441759
David Jones <david.i.jones@infinityworks.com>
2023-09-20 13:34:20 +0100

    Initial Commit

---
+++ Hackathon/HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonApp.swift
+//  Hackathon
+//
+//  Created by David Jones on 20/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
