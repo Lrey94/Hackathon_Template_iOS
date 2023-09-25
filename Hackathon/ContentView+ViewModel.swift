d5894f5e3b1cbbabfbaceeeab8cc7143ca574513
Lrey94 <reynoldslawrence21@yahoo.co.uk>
2023-09-25 11:13:24 +0100

    first commit

---
+++ Hackathon/ContentView+ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentView+ViewModel.swift
+//  Hackathon
+//
+//  Created by David Jones on 20/09/2023.
+//
+
+import Foundation
---
+++ Hackathon/DataModel.swift
@@ -0,0 +1,15 @@
+//
+//  CharacterModel.swift
+//  Hackathon
+//
+//  Created by David Jones on 20/09/2023.
+//
+
+import Foundation
+
+public struct DataModel: Codable {
+    
+    public let name: String
+    
+    
+}
